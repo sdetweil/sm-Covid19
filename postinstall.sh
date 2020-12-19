43dad6244fcafcede60949e8bbfe16af06873c4c
Sam Detweiler <sdetweil@gmail.com>
2020-12-19 13:50:28 -0600

    add support for dynamic nls detection

---
+++ postinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cp .pre-commit .git/hooks/pre-commit
+fs=$(stat -c%s ../../app/locales/en.json)
+if [ $fs -lt 1000 ]; then
+	# nls enabled
+	source=config.schema.json.nls
+	# if the compiled trans files exist
+	if [ -e ../../app/locales/enc.json ]; then
+	    # erase so they will be reqbuilt
+	  echo	rm ../../app/locales/??c.json
+	fi
+else
+	# nls NOT enabled
+	source=config.schema.json.no-nls
+fi
+cp $source config.schema.json
